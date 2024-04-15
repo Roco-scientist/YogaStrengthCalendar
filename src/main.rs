db049f507819849c01b6f06cacfbeec84f6879ab
Rory Coffey <coffeyrt@gmail.com>
2024-04-15 08:21:35 -0400

    changed size

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,8 @@ fn main() {
 
     let native_options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
-            .with_inner_size([400.0, 600.0])
-            .with_min_inner_size([300.0, 220.0])
+            .with_inner_size([360.0, 800.0])
+            .with_min_inner_size([360.0, 220.0])
             .with_icon(
                 eframe::icon_data::from_png_bytes(&include_bytes!("../bike.png")[..]).unwrap(),
             ),
